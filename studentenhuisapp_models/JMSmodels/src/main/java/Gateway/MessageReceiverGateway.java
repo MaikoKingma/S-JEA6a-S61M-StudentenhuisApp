7e68d6281b0813bab600c876a63b15af07a1060b
Maiko kingma <maiko.kingma@outlook.com>
2017-09-07 16:56:40 +0200

    Created and implemented MessageReceiverGateway.java

---
+++ studentenhuisapp_models/JMSmodels/src/main/java/Gateway/MessageReceiverGateway.java
@@ -0,0 +1,45 @@
+package Gateway;
+
+import javax.jms.*;
+import javax.naming.*;
+import java.util.Properties;
+
+public class MessageReceiverGateway {
+    private final String INITIAL_CONTEXT_FACTORY = "org.apache.activemq.jndi.ActiveMQInitialContextFactory";
+    private final String PROVIDER_URL = "tcp://localhost:61616";
+
+    private Connection connection;
+    private Session session;
+    private Destination destination;
+    private MessageConsumer consumer;
+
+    public MessageReceiverGateway(String channelName) {
+        Properties props = new Properties();
+        props.setProperty(Context.INITIAL_CONTEXT_FACTORY, INITIAL_CONTEXT_FACTORY);
+        props.setProperty(Context.PROVIDER_URL, PROVIDER_URL);
+        // connect to the Destination
+        props.put(("queue." + channelName), channelName);
+
+        try {
+            Context jndiContext = new InitialContext(props);
+            ConnectionFactory connectionFactory = (ConnectionFactory) jndiContext.lookup("ConnectionFactory");
+            connection = connectionFactory.createConnection();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            // connect to the receiver destination
+            destination = (Destination) jndiContext.lookup(channelName);
+            consumer = session.createConsumer(destination);
+            connection.start(); // this is needed to start receiving messages
+        } catch (NamingException | JMSException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setListener(MessageListener listener) {
+        try {
+            consumer.setMessageListener(listener);
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
