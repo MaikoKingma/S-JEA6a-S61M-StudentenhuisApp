563146ad0f931188d691918439f56ddea659ca3f
Maiko kingma <maiko.kingma@outlook.com>
2017-09-07 16:20:37 +0200

    Squashed commit of the following:
    commit 57b9783ac5cae7fe2d47055d2bc5ff6518738c26
    Author: Maiko kingma <maiko.kingma@outlook.com>
    Date:   Thu Sep 7 16:12:42 2017 +0200
    
        Created model libraries and added them to the main projects

---
+++ studentenhuisapp_models/models/src/User.java
@@ -0,0 +1,16 @@
+public class User {
+
+    private long ID;
+
+    public User(long ID) {
+        this.ID = ID;
+    }
+
+    public long getID() {
+        return ID;
+    }
+
+    public void setID(long ID) {
+        this.ID = ID;
+    }
+}
