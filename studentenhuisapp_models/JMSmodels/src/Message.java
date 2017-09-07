df3b151f7b6033f704cd488fa5189b7587cc103a
Maiko kingma <maiko.kingma@outlook.com>
2017-09-07 16:35:40 +0200

    Created message model

--- studentenhuisapp_models/JMSmodels/src/Message.java
+++ studentenhuisapp_models/JMSmodels/src/Message.java
@@ -1,3 +1,56 @@
+import java.util.Calendar;
+
 public class Message {
 
+    private long id;
+    private Calendar date;
+    private long groupId;
+    private long userId;
+    private Events event;
+
+    public Message(long id, Calendar date, Events event) {
+        this.id = id;
+        this.date = date;
+        this.event = event;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Calendar getDate() {
+        return date;
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(long groupId) {
+        this.groupId = groupId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(long userId) {
+        this.userId = userId;
+    }
+
+    public Events getEvent() {
+        return event;
+    }
+
+    public void setEvent(Events event) {
+        this.event = event;
+    }
 }
