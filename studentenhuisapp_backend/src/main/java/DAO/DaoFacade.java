4e01ec5a4bbb1166b216b69e681ace6815623093
Maiko kingma <maiko.kingma@outlook.com>
2017-09-08 15:22:44 +0200

    Refractor

--- studentenhuisapp_backend/src/main/java/DAO/DaoFacade.java
+++ studentenhuisapp_backend/src/main/java/DAO/DaoFacade.java
@@ -1,40 +1,36 @@
 package DAO;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 public abstract class DaoFacade<T> {
-    private  final Class<T> entityClass;
+    private final Class<T> entityClass;
 
-    public DaoFacade(Class<T> entityClass)
-    {
+    @PersistenceContext
+    protected EntityManager em;
+
+    public DaoFacade(Class<T> entityClass) {
         this.entityClass = entityClass;
     }
 
-    protected abstract EntityManager getEntityManager();
-
-    public void create(T entity)
-    {
-        getEntityManager().persist(entity);
+    public void create(T entity) {
+        em.persist(entity);
     }
 
-    public void edit(T entity)
-    {
-        getEntityManager().merge(entity);
+    public void edit(T entity) {
+        em.merge(entity);
     }
 
-    public void remove(T entity)
-    {
-        getEntityManager().remove(getEntityManager().merge(entity));
+    public void remove(T entity) {
+        em.remove(em.merge(entity));
     }
 
-    public T findById(Object id)
-    {
-        return getEntityManager().find(entityClass, id);
+    public T findById(Object id) {
+        return em.find(entityClass, id);
     }
 
-    public List<T> getAll()
-    {
-        return getEntityManager().createQuery("select t from " + entityClass.getSimpleName() + " t").getResultList();
+    public List<T> getAll() {
+        return em.createQuery("select t from " + entityClass.getSimpleName() + " t").getResultList();
     }
 }
