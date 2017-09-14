41e51b96d41d7285c882d35fbbac11b5391fdc03
Maiko kingma <maiko.kingma@outlook.com>
2017-09-14 11:36:58 +0200

    add basic rest

---
+++ studentenhuisapp_backend/src/main/java/Controllers/Studentenhuisapp.java
@@ -0,0 +1,21 @@
+package Controllers;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.Set;
+
+@ApplicationPath("/*")
+public class Studentenhuisapp extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(CORSFilter.class);
+        resources.add(UserController.class);
+    }
+}
