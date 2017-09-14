da34a3aef0217bf706dcc12a87f4f09a34e5bdc1
Maiko kingma <maiko.kingma@outlook.com>
2017-09-14 11:28:18 +0200

    Check if JMSBroker is used

--- studentenhuisapp_backend/src/test/java/Service/UserServiceTest.java
+++ studentenhuisapp_backend/src/test/java/Service/UserServiceTest.java
@@ -1,14 +1,17 @@
 package Service;
 
 import DAO.IUserDao;
+import JMS.JMSBrokerGateway;
+import JMS.Message.Events;
 import Models.User;
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.mockito.Mockito.*;
+
+//Note: always check if events are send to the JMS broker
 @RunWith(MockitoJUnitRunner.class)
 public class UserServiceTest {
 
@@ -17,10 +20,14 @@ public class UserServiceTest {
 
     @Mock
     private IUserDao userDao;
+    @Mock
+    private JMSBrokerGateway jmsBroker;
 
     @Test
     public void createUserTest() throws Exception {
         final User testUser = new User("Maiko", "maiko@mail.nl");
         Assert.assertEquals(testUser, userService.create(testUser));
+        verify(jmsBroker, times(1))
+                .sendMessage(Mockito.anyString(), eq(Events.ACCOUNT_CREATED), eq(testUser.getId()));
     }
 }
