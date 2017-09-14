d21b8223c4b576d39a21fa7c76014eeedf03d14d
Maiko kingma <maiko.kingma@outlook.com>
2017-09-14 11:28:07 +0200

    Add JMSBroker

--- studentenhuisapp_backend/src/main/java/Service/UserService.java
+++ studentenhuisapp_backend/src/main/java/Service/UserService.java
@@ -1,6 +1,8 @@
 package Service;
 
 import DAO.IUserDao;
+import JMS.JMSBrokerGateway;
+import JMS.Message.Events;
 import Models.User;
 
 import javax.ejb.Stateless;
@@ -11,9 +13,12 @@ public class UserService {
 
     @Inject
     private IUserDao userDao;
+    @Inject
+    private JMSBrokerGateway jmsBroker;
 
     public User create(User user) {
         userDao.create(user);
+        jmsBroker.sendMessage("New Account Created", Events.ACCOUNT_CREATED, user.getId());
         return user;
     }
 }
