baeb501a42b8e7091f0018a8e37eea95f341c01c
Maiko kingma <maiko.kingma@outlook.com>
2017-09-22 09:53:47 +0200

    Minor changes to unittests

--- studentenhuisapp_backend/src/test/java/Controller/AccountControllerTest.java
+++ studentenhuisapp_backend/src/test/java/Controller/AccountControllerTest.java
@@ -10,15 +10,14 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
-import javax.ws.rs.Path;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.*;
 
 import static java.net.HttpURLConnection.HTTP_CREATED;
 import static java.net.HttpURLConnection.HTTP_INTERNAL_ERROR;
-import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AccountControllerTest extends JerseyTest {
@@ -39,38 +38,48 @@ public class AccountControllerTest extends JerseyTest {
     }
 
     @Test
-    public void createUserTest() throws Exception {
-        final Account testAccount = new Account("Maiko", "maiko999@mail.nl");
-        when(service.create(testAccount))
-                .thenReturn(testAccount);
+    public void createAccountTest() throws Exception {
+            final Account testAccount = new Account("Maiko", "maiko999@mail.nl");
+            Mockito.when(service.create(testAccount))
+                    .thenReturn(testAccount);
 
-        System.out.println("Create account");
-        //Create a new Account
-        final Response correctResult = target("/accounts")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.json(testAccount));
-        Assert.assertEquals(HTTP_CREATED, correctResult.getStatus());
-        Assert.assertEquals(testAccount, correctResult.readEntity(Account.class));
+            System.out.println("Create account");
+            //Create a new Account
+            final Response correctResult = target("/accounts")
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.json(testAccount));
+            Assert.assertEquals("Create Account did not give the correct response.",
+                    HTTP_CREATED,
+                    correctResult.getStatus());
+            Assert.assertEquals("Create Account did not return the correct account.",
+                    testAccount,
+                    correctResult.readEntity(Account.class));
 
-        System.out.println("Create duplicate account");
-        //Try to create a duplicate product
-        final Response duplicateResult = target("/accounts")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.json(testAccount));
-        Assert.assertEquals(HTTP_INTERNAL_ERROR, duplicateResult.getStatus());
+            System.out.println("Create duplicate account");
+            //Try to create a duplicate product
+            final Response duplicateResult = target("/accounts")
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.json(testAccount));
+            Assert.assertEquals("Duplicate account was created.",
+                    HTTP_INTERNAL_ERROR,
+                    duplicateResult.getStatus());
 
-        System.out.println("Create empty account");
-        //Try to create a new empty product
-        final Response faultyResult = target("/accounts")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.json(""));
-        Assert.assertEquals(HTTP_INTERNAL_ERROR, faultyResult.getStatus());
+            System.out.println("Create empty account");
+            //Try to create a new empty product
+            final Response faultyResult = target("/accounts")
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.json(""));
+            Assert.assertEquals("Empty account was created",
+                    HTTP_INTERNAL_ERROR,
+                    faultyResult.getStatus());
 
-        System.out.println("Create null account");
-        //Try to create a new null product
-        final Response faultyResult2 = target("/accounts")
-                .request(MediaType.APPLICATION_JSON)
-                .post(Entity.json(null));
-        Assert.assertEquals(HTTP_INTERNAL_ERROR, faultyResult2.getStatus());
+            System.out.println("Create null account");
+            //Try to create a new null product
+            final Response faultyResult2 = target("/accounts")
+                    .request(MediaType.APPLICATION_JSON)
+                    .post(Entity.json(null));
+            Assert.assertEquals("Null account was created",
+                    HTTP_INTERNAL_ERROR,
+                    faultyResult2.getStatus());
     }
 }
--- studentenhuisapp_backend/src/test/java/DAO/AccountDaoJPATest.java
+++ studentenhuisapp_backend/src/test/java/DAO/AccountDaoJPATest.java
@@ -10,6 +10,8 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import javax.persistence.EntityManager;
 
+import static com.github.stefanbirkner.fishbowl.Fishbowl.exceptionThrownBy;
+
 @RunWith(MockitoJUnitRunner.class)
 public class AccountDaoJPATest {
 
@@ -23,41 +25,23 @@ public class AccountDaoJPATest {
 
     @Test
     public void createUserTest() throws Exception {
-        System.out.println("Test create user");
-        //Create a user and test if the values are correct
         final Account correctAccount = new Account("Maiko", "maiko@mail.nl");
-        Assert.assertEquals(correctAccount, productDao.create(correctAccount));
-        Assert.assertTrue(correctAccount.isActive());
+        Assert.assertEquals("User not created.",
+                correctAccount,
+                productDao.create(correctAccount));
+        Assert.assertTrue("New user is not active.",
+                correctAccount.isActive());
 
-//        System.out.println("Test duplicate user");
-//        //Check if duplicate mail adresses are allowed.
-//        final Account duplicateUser = new Account("Maiko", "maiko@mail.nl");
-//        Assert.assertEquals(null, duplicateUser);
+        Assert.assertNotNull("Empty name was accepted.",
+                exceptionThrownBy(() -> productDao.create(new Account("", "pim@mail.nl"))));
 
-        System.out.println("Test empty name");
-        try {
-            productDao.create(new Account("", "pim@mail.nl"));
-            Assert.fail("Empty name should not be accepted.");
-        } catch (NullPointerException e) { }
+        Assert.assertNotNull("Empty mail was accepted",
+                exceptionThrownBy(() -> productDao.create(new Account("Pim", ""))));
 
-        System.out.println("Test empty mail");
-        try {
-            productDao.create(new Account("Pim", ""));
-            Assert.fail("Empty mail should not be accepted.");
-        } catch (NullPointerException e) { }
+        Assert.assertNotNull("Null name was accepted",
+                exceptionThrownBy(() -> productDao.create(new Account(null, "pim@mail.nl"))));
 
-        System.out.println("Test null name");
-        try {
-            productDao.create(new Account(null, "pim@mail.nl"));
-            Assert.fail("Null name should not be accepted.");
-        } catch (NullPointerException e) { }
-
-        System.out.println("Test null mail");
-        try {
-            productDao.create(new Account("Pim", null));
-            Assert.fail("Null mail should not be accepted.");
-        } catch (NullPointerException e) { }
-
-        //ToDo test duplicate name
+        Assert.assertNotNull("Null mail was accepted",
+                exceptionThrownBy(() -> productDao.create(new Account("Pim", null))));
     }
 }
