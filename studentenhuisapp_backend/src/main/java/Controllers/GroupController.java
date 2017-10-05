01749e56d2279f4f7dfbb2f02360ca34efd32932
Maiko kingma <maiko.kingma@outlook.com>
2017-10-05 12:17:04 +0200

    add createGroupController with unit test

---
+++ studentenhuisapp_backend/src/main/java/Controllers/GroupController.java
@@ -0,0 +1,30 @@
+package Controllers;
+
+import Models.Group;
+import Service.GroupService;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.*;
+import java.net.URI;
+
+@Stateless
+@Path("/groups")
+public class GroupController {
+
+    @Inject
+    private GroupService groupService;
+
+    @Context
+    private UriInfo uriInfo;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response create(Group group) {
+        final Group newGroup = groupService.create(group);
+        final URI uri = uriInfo.getAbsolutePathBuilder().path(newGroup.getId() + "").build();
+        return Response.created(uri).entity(newGroup).build();
+    }
+}
---
+++ studentenhuisapp_backend/src/test/java/Controller/GroupControllerTest.java
@@ -0,0 +1,68 @@
+package Controller;
+
+import Controllers.GroupController;
+import Models.*;
+import Service.GroupService;
+import org.glassfish.jersey.internal.inject.AbstractBinder;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.*;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.*;
+
+import static java.net.HttpURLConnection.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GroupControllerTest extends JerseyTest {
+
+    @Mock
+    private GroupService service;
+
+    @Override
+    protected Application configure() {
+        final ResourceConfig config = new ResourceConfig(GroupController.class);
+        config.register(new AbstractBinder() {
+            @Override
+            protected void configure() {
+                bind(service).to(GroupService.class);
+            }
+        });
+        return config;
+    }
+
+    @Test
+    public void createAccountTest() throws Exception {
+        final Account account = new Account("Maiko", "maiko999@mail.nl");
+        final Group testGroup = new Group("Studentenhuis", account);
+        Mockito.when(service.create(testGroup))
+                .thenReturn(testGroup);
+
+        final Response correctResult = target("/groups")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(testGroup));
+        Assert.assertEquals("Create Group did not give the correct response.",
+                HTTP_CREATED,
+                correctResult.getStatus());
+        Assert.assertEquals("Create Group did not return the correct group.",
+                testGroup,
+                correctResult.readEntity(Group.class));
+
+        final Response faultyResult = target("/groups")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(""));
+        Assert.assertEquals("Empty group was created",
+                HTTP_INTERNAL_ERROR,
+                faultyResult.getStatus());
+
+        final Response faultyResult2 = target("/groups")
+                .request(MediaType.APPLICATION_JSON)
+                .post(Entity.json(null));
+        Assert.assertEquals("Null group was created",
+                HTTP_INTERNAL_ERROR,
+                faultyResult2.getStatus());
+    }
+}
